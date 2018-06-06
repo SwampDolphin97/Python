dd7856742243f9c161c3996869d2369488cf71e0
Karan Raheja <karan@localhost.localdomain>
2018-06-06 11:21:09 +0530

    string format string

---
+++ string_format.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+age=20
+name='Karan'
+print('{1} was {0} year old when he started learning python'.format(age, name))
+print('hello, It\'s good if that boy whose name is {0} is learning python'.format(name))
+
+
+
