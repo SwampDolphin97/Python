6375092c48c6ca73d6ecfa60972f28b3bb44a904
Karan Raheja <karan@localhost.localdomain>
2018-06-07 10:38:37 +0530

    while loop

--- while.py
+++ while.py
@@ -14,4 +14,10 @@ else :
     print('The while loop is over.')
 print('Done.')
 
-
+#Break statement is used to break out of a loop statement. or to stop the execution of the current looping instruction.
+#while True :
+#s = input('Enter Something : ')
+#if s == 'quit' :
+#   break;
+#printf('Length of the string is', len(s))
+#printf('Done')
