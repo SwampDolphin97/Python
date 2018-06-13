4ac2bc5521e874efba41afe046526b77550f11fb
Karan Raheja <karan@localhost.localdomain>
2018-06-13 10:49:11 +0530

    banner grabbing script using functions

--- security-python/bangrabfunc.py
+++ security-python/bangrabfunc.py
@@ -13,3 +13,12 @@ def retbanner(ip, port):
         ip1='<IP-ADDRESS>'
         ip2='<IP-ADDRESS>'
         port=21
+banner1 = retbanner(ip1, port)
+if banner1:
+    print '[+]' + ip1 + ':' + banner1
+banner2 = retbanner(ip2, port)
+if banner2:
+    print '[+]' + ip2 + ':' + banner2
+if __name__=='__main__':
+    main()
+
